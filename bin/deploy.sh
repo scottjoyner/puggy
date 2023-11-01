ae17d87b295795468c0106e91783f44b305e932e
Scott Joyner <kipnerter@gmail.com>
2023-11-01 17:30:06 -0400

    Update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
-cd /kipnerter/puggy/
-git pull --no-edit
-cp /kipnerter/puggy/docs/* /var/www/html
-cp /kipnerter/puggy/bin/* /kipnerter/bin
-chown -R caddy:www-data /var/www/html
-chmod 744 /var/www/html/*
-chmod 744 /var/www/html/*/*
-chmod 744 /var/www/html/*/*/*
-echo Complete
+# Define the log file location
+LOG_FILE="/var/log/my_script.log"
+
+{
+    # Change to the required directory
+    cd /kipnerter/puggy/ || exit 1
+
+    # Pull the latest changes from the git repository
+    git pull --no-edit
+
+    # Copy the files to the appropriate locations
+    cp /kipnerter/puggy/docs/* /var/www/html
+    cp /kipnerter/puggy/bin/* /kipnerter/bin
+
+    # Change the ownership of the files
+    chown -R caddy:www-data /var/www/html
+
+    # Set the appropriate permissions
+    chmod 744 /var/www/html/*
+    chmod 744 /var/www/html/*/*
+    chmod 744 /var/www/html/*/*/*
+
+    # Print completion message
+    echo "Complete"
+
+} >> "$LOG_FILE" 2>&1
