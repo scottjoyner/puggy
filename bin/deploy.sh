97c208a07f6d7bc0b4bd8547448a9a41c3f1fddd
Scott Joyner <kipnerter@gmail.com>
2023-11-01 17:28:11 -0400

    Update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 cd /kipnerter/puggy/
-git pull
+git pull --no-edit
 cp /kipnerter/puggy/docs/* /var/www/html
 cp /kipnerter/puggy/bin/* /kipnerter/bin
 chown -R caddy:www-data /var/www/html
 chmod 744 /var/www/html/*
 chmod 744 /var/www/html/*/*
 chmod 744 /var/www/html/*/*/*
+echo Complete
